97a248dfd5b6f362d17bf0930c54a07f50b3f987
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2020-12-08 17:41:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -362,10 +362,24 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
-  }
-  
+// function grade(/*Your Code here */){
+//   /*Your Code here */
+//   }
+function grade(score){
+  if(score >= 90){
+  return 'you got a A'
+} else if (score >= 80){
+  return 'you got a B'
+} else if (score >= 70){
+  return 'you got a C'
+} else if (score >=60){
+  return 'you got a D'
+} else {
+  return 'you got a F'
+}
+}
+
+console.log(grade(90));
   
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
